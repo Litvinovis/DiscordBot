4fbee04f9955c0ecb3d6f2fa97ecc3f33eab9236
Litvinov igor <litvinovis102@yandex.ru>
2023-10-07 00:31:44 +0300

    Подключаем тинькофф АПИ

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,13 +3,15 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import ru.tinkoff.piapi.core.InvestApi;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
 
+        InvestApi api = InvestApi.createReadonly("t.xKX9aDEMB8xngn3y3rHeG3bc7DSzWOd2BZVZy_cOd5WymFsjedRQ0SN5pKeKC8Y7NH0CeiQB1M7JQN13s_VgJA");
         JDA jda = JDABuilder.createDefault("MTE1NzA1MjkxOTkwOTIwMzk5OQ.GW2drl.k086vq1RgkU9zCqor7S7ePkcI-WoCSJL0czWW0")
-                .addEventListeners(new MessageHandler())
+                .addEventListeners(new MessageHandler(api))
                 .setActivity(Activity.playing("NASDAQ"))
                 .build();
     }
--- src/main/java/EventHandlers/MessageHandler.java
+++ src/main/java/EventHandlers/MessageHandler.java
@@ -1,30 +1,48 @@
 package EventHandlers;
 
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ru.tinkoff.piapi.core.InvestApi;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 @Slf4j
 public class MessageHandler extends ListenerAdapter {
+  private final InvestApi api;
 
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.getAuthor().isBot() && event.getChannel().getName().equals("основной")) {
-            try {
-                event.getChannel().sendMessage("Согласен на все 100!")
-                        .delay(5, TimeUnit.SECONDS)
-                        .flatMap(message -> message.editMessage("хотя нет, передумал =)"))
-                        .delay(5, TimeUnit.SECONDS)
-                        .flatMap(Message::delete)
-                        .delay(1, TimeUnit.SECONDS)
-                        .flatMap(message -> event.getMessage().delete())
-                        .submit();
-            } catch (Exception e) {
-                log.error(Constants.LOG_MESSAGE, e.getMessage());
-            }
+  public MessageHandler(InvestApi api) {
+    this.api = api;
+  }
+
+  @Override
+  public void onMessageReceived(MessageReceivedEvent event) {
+    try {
+      if (event.getChannel().getName().equals("основной")) {
+        if (event.getAuthor().getName().equals("chegobnk") && random()) {
+          event.getChannel().sendMessage("О, мсье чегобник как дела?")
+                  .submit();
+        } else if (event.getAuthor().getName().equals("kakimito") && random()) {
+          event.getChannel().sendMessage("Kakimito, опять ты чушь несёшь?")
+                  .submit();
+        } else if (event.getAuthor().getName().equals("l4rover") && random()) {
+          event.getChannel().sendMessage("О создатель, ты в чате!")
+                  .submit();
+        } else if (event.getAuthor().getName().equals("david orson") && random()) {
+          event.getChannel().sendMessage("А можно форточку открыть?")
+                  .submit();
+        } else if (event.getAuthor().getName().equals("я бы взял") && random()) {
+          event.getChannel().sendMessage("аргументный аргумент конечно")
+                  .submit();
         }
+      }
+    } catch (Exception e) {
+      log.error(Constants.LOG_MESSAGE, e.getMessage());
     }
+  }
+
+  private boolean random() {
+    Random random = new Random();
+    return random.nextBoolean();
+  }
 }
