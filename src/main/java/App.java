9374b24d54babac64abfe319f10ef7f3b91372da
Litvinov igor <litvinovis102@yandex.ru>
2023-10-07 20:24:15 +0300

    Starting develop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ import ru.tinkoff.piapi.core.InvestApi;
 @Slf4j
 public class App {
     public static void main(String[] args) {
-
+        log.trace("Стартуем приложение");
         InvestApi api = InvestApi.createReadonly("t.xKX9aDEMB8xngn3y3rHeG3bc7DSzWOd2BZVZy_cOd5WymFsjedRQ0SN5pKeKC8Y7NH0CeiQB1M7JQN13s_VgJA");
         JDA jda = JDABuilder.createDefault("MTE1NzA1MjkxOTkwOTIwMzk5OQ.GW2drl.k086vq1RgkU9zCqor7S7ePkcI-WoCSJL0czWW0")
                 .addEventListeners(new MessageHandler(api))
