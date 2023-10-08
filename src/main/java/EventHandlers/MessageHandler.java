f9929913ab9d6caa81b2116d09926f09a5eea3f1
Litvinov igor <litvinovis102@yandex.ru>
2023-10-08 18:59:49 +0300

    Добавлена функция запроса стоимости акций

--- src/main/java/EventHandlers/MessageHandler.java
+++ src/main/java/EventHandlers/MessageHandler.java
@@ -5,9 +5,12 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import ru.tinkoff.piapi.contract.v1.Currency;
+import ru.tinkoff.piapi.contract.v1.LastPrice;
+import ru.tinkoff.piapi.contract.v1.Share;
 import ru.tinkoff.piapi.core.InvestApi;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class MessageHandler extends ListenerAdapter {
@@ -20,9 +23,12 @@ public class MessageHandler extends ListenerAdapter {
   @Override
   public void onMessageReceived(@NotNull MessageReceivedEvent event) {
     try {
-      if (event.getChannel().getName().equals("ботный")) {
-        if (event.getAuthor().getName().equals("l4rover") && event.getMessage().getContentDisplay().contains("+валюта")) {
+      if (isBotAsking(event)) {
+        if (event.getMessage().getContentDisplay().contains("+валюта")) {
           event.getChannel().sendMessage(getCurrencyInfo(event.getMessage().getContentDisplay().substring(8)))
+                  .submit();
+        } else if (event.getMessage().getContentDisplay().contains("+акция")) {
+          event.getChannel().sendMessage(getSharesInfo(event.getMessage().getContentDisplay().substring(7)))
                   .submit();
         }
       }
@@ -51,5 +57,31 @@ public class MessageHandler extends ListenerAdapter {
       return "Сорян я глючу";
     }
     return builder.toString();
+  }
+
+  public String getSharesInfo(String sharesName) {
+    StringBuilder builder = new StringBuilder("Информация о подходящих акциях: \n");
+    try {
+      List<Share> shares = api.getInstrumentsService().getAllSharesSync().stream()
+              .filter(share -> share.getName().toLowerCase().contains(sharesName.toLowerCase()))
+              .filter(Share::getBuyAvailableFlag)
+              .toList();
+      List<LastPrice> lastPrices = api.getMarketDataService().getLastPricesSync(shares.stream().map(Share::getFigi).collect(Collectors.toList()));
+
+      for (int i = 0; i < shares.size(); i++) {
+        builder.append("\n").append("Название: ").append(shares.get(i).getName()).append("\n")
+                .append("Стоимость = ").append(lastPrices.get(i).getPrice().getUnits()).append(",")
+                .append(lastPrices.get(i).getPrice().getNano() > 100 ? String.valueOf(lastPrices.get(i).getPrice().getNano()).substring(0, 2) : lastPrices.get(i).getPrice().getNano())
+                .append(" ").append(shares.get(i).getCurrency().toUpperCase()).append("\n");
+      }
+    } catch (Exception e) {
+      log.trace("Ошибка {}", e.getMessage());
+      return "Сорян я глючу";
+    }
+    return builder.toString();
+  }
+
+  private boolean isBotAsking(MessageReceivedEvent event) {
+    return event.getMessage().getContentDisplay().startsWith("+");
   }
 }
