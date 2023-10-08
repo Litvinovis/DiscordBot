a44123b2de0942479a2ac5f58b4bb043ae491cef
Litvinov igor <litvinovis102@yandex.ru>
2023-10-08 23:59:36 +0300

    Добавлена функция запроса стоимости акций

--- src/main/java/EventHandlers/MessageHandler.java
+++ src/main/java/EventHandlers/MessageHandler.java
@@ -10,6 +10,7 @@ import ru.tinkoff.piapi.contract.v1.Share;
 import ru.tinkoff.piapi.core.InvestApi;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -39,9 +40,19 @@ public class MessageHandler extends ListenerAdapter {
 
   public String getCurrencyInfo(String currency) {
     StringBuilder builder = new StringBuilder("Информация о валюте ").append(currency).append(":\n");
+    List<Currency> currencies = null;
     try {
-      List<Currency> currencies = api.getInstrumentsService().getAllCurrencies().get();
-      List<String> filtered = currencies.stream()
+      currencies = api.getInstrumentsService().getAllCurrencies().get();
+    } catch (InterruptedException e) {
+      System.out.println(e.getCause());
+      System.out.println(e.getMessage());
+      throw new RuntimeException(e);
+    } catch (ExecutionException e) {
+      System.out.println(e.getCause());
+      System.out.println(e.getMessage());
+      throw new RuntimeException(e);
+    }
+    List<String> filtered = currencies.stream()
               .filter(currency1 -> currency1.getIsoCurrencyName().equalsIgnoreCase(currency))
               .map(Currency::getFigi)
               .toList();
@@ -52,34 +63,24 @@ public class MessageHandler extends ListenerAdapter {
               .append(String.valueOf(lastPrices.get(0).getPrice().getNano()), 0, 2)
               .append(" рублей за 1 ")
               .append(currency);
-    } catch (Exception e) {
-      log.trace("Ошибка {}", e.getMessage());
-      return "Сорян я глючу";
-    }
     return builder.toString();
   }
 
   public String getSharesInfo(String sharesName) {
     StringBuilder builder = new StringBuilder("Информация о подходящих акциях: \n");
     try {
-      System.out.println(1);
       List<Share> shares = api.getInstrumentsService().getAllSharesSync().stream()
               .filter(share -> share.getName().toLowerCase().contains(sharesName.toLowerCase()))
               .toList();
-      System.out.println(2);
       List<LastPrice> lastPrices = api.getMarketDataService().getLastPricesSync(shares.stream().map(Share::getFigi).collect(Collectors.toList()));
-      System.out.println(3);
       for (int i = 0; i < shares.size(); i++) {
-        System.out.println(4);
         builder.append("\n").append("Название: ").append(shares.get(i).getName()).append("\n")
                 .append("Стоимость = ").append(lastPrices.get(i).getPrice().getUnits()).append(",")
                 .append(lastPrices.get(i).getPrice().getNano() > 100 ? String.valueOf(lastPrices.get(i).getPrice().getNano()).substring(0, 2) : lastPrices.get(i).getPrice().getNano())
                 .append(" ").append(shares.get(i).getCurrency().toUpperCase()).append("\n");
       }
     } catch (Exception e) {
-      log.trace("Ошибка {}", e.getMessage());
       System.out.println(e.getMessage());
-      System.out.println(e);
       return "Сорян я глючу " + e.getMessage();
     }
     return builder.toString();
