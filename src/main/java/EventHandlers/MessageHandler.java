13d4c800d49a7ad322ec132259ce7f20f648cb8b
Litvinov igor <litvinovis102@yandex.ru>
2023-10-08 23:22:08 +0300

    Добавлена функция запроса стоимости акций

--- src/main/java/EventHandlers/MessageHandler.java
+++ src/main/java/EventHandlers/MessageHandler.java
@@ -62,13 +62,16 @@ public class MessageHandler extends ListenerAdapter {
   public String getSharesInfo(String sharesName) {
     StringBuilder builder = new StringBuilder("Информация о подходящих акциях: \n");
     try {
+      System.out.println(1);
       List<Share> shares = api.getInstrumentsService().getAllSharesSync().stream()
               .filter(Share::getBuyAvailableFlag)
               .filter(share -> share.getName().toLowerCase().contains(sharesName.toLowerCase()))
               .toList();
+      System.out.println(2);
       List<LastPrice> lastPrices = api.getMarketDataService().getLastPricesSync(shares.stream().map(Share::getFigi).collect(Collectors.toList()));
-
+      System.out.println(3);
       for (int i = 0; i < shares.size(); i++) {
+        System.out.println(4);
         builder.append("\n").append("Название: ").append(shares.get(i).getName()).append("\n")
                 .append("Стоимость = ").append(lastPrices.get(i).getPrice().getUnits()).append(",")
                 .append(lastPrices.get(i).getPrice().getNano() > 100 ? String.valueOf(lastPrices.get(i).getPrice().getNano()).substring(0, 2) : lastPrices.get(i).getPrice().getNano())
