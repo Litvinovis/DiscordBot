fcef01fd26a6962468cd904a1dc040afbcfd5b84
Litvinov igor <litvinovis102@yandex.ru>
2023-10-08 14:56:34 +0300

    Adding gitignore

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import ru.tinkoff.piapi.core.InvestApi;
 
 @Slf4j
@@ -11,6 +12,7 @@ public class App {
         log.trace("Стартуем приложение");
         InvestApi api = InvestApi.createReadonly("t.xKX9aDEMB8xngn3y3rHeG3bc7DSzWOd2BZVZy_cOd5WymFsjedRQ0SN5pKeKC8Y7NH0CeiQB1M7JQN13s_VgJA");
         JDA jda = JDABuilder.createDefault("MTE1NzA1MjkxOTkwOTIwMzk5OQ.GW2drl.k086vq1RgkU9zCqor7S7ePkcI-WoCSJL0czWW0")
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new MessageHandler(api))
                 .setActivity(Activity.playing("NASDAQ"))
                 .build();
--- src/main/java/EventHandlers/MessageHandler.java
+++ src/main/java/EventHandlers/MessageHandler.java
@@ -3,9 +3,11 @@ package EventHandlers;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import ru.tinkoff.piapi.contract.v1.Currency;
 import ru.tinkoff.piapi.core.InvestApi;
 
-import java.util.Random;
+import java.util.List;
 
 @Slf4j
 public class MessageHandler extends ListenerAdapter {
@@ -16,23 +18,11 @@ public class MessageHandler extends ListenerAdapter {
   }
 
   @Override
-  public void onMessageReceived(MessageReceivedEvent event) {
+  public void onMessageReceived(@NotNull MessageReceivedEvent event) {
     try {
-      if (event.getChannel().getName().equals("основной")) {
-        if (event.getAuthor().getName().equals("chegobnk") && random()) {
-          event.getChannel().sendMessage("О, мсье чегобник как дела?")
-                  .submit();
-        } else if (event.getAuthor().getName().equals("kakimito") && random()) {
-          event.getChannel().sendMessage("Kakimito, опять ты чушь несёшь?")
-                  .submit();
-        } else if (event.getAuthor().getName().equals("l4rover") && random()) {
-          event.getChannel().sendMessage("О создатель, ты в чате!")
-                  .submit();
-        } else if (event.getAuthor().getName().equals("david orson") && random()) {
-          event.getChannel().sendMessage("А можно форточку открыть?")
-                  .submit();
-        } else if (event.getAuthor().getName().equals("я бы взял") && random()) {
-          event.getChannel().sendMessage("аргументный аргумент конечно")
+      if (event.getChannel().getName().equals("ботный")) {
+        if (event.getAuthor().getName().equals("l4rover") && event.getMessage().getContentDisplay().contains("+валюта")) {
+          event.getChannel().sendMessage(getCurrencyInfo(event.getMessage().getContentDisplay().substring(8)))
                   .submit();
         }
       }
@@ -41,8 +31,25 @@ public class MessageHandler extends ListenerAdapter {
     }
   }
 
-  private boolean random() {
-    Random random = new Random();
-    return random.nextBoolean();
+  public String getCurrencyInfo(String currency) {
+    StringBuilder builder = new StringBuilder("Информация о валюте ").append(currency).append(":\n");
+    try {
+      List<Currency> currencies = api.getInstrumentsService().getAllCurrencies().get();
+      List<String> filtered = currencies.stream()
+              .filter(currency1 -> currency1.getIsoCurrencyName().equalsIgnoreCase(currency))
+              .map(Currency::getFigi)
+              .toList();
+      var lastPrices = api.getMarketDataService().getLastPricesSync(filtered);
+      builder.append("Курс = ")
+              .append(lastPrices.get(0).getPrice().getUnits())
+              .append(",")
+              .append(String.valueOf(lastPrices.get(0).getPrice().getNano()), 0, 2)
+              .append(" рублей за 1 ")
+              .append(currency);
+    } catch (Exception e) {
+      log.trace("Ошибка {}", e.getMessage());
+      return "Сорян я глючу";
+    }
+    return builder.toString();
   }
 }
